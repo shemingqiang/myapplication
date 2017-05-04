ab60734fc0f12aa00bb2fd80e5ae6f3838131a05
shemingqinag <17300142501@163.com>
2017-05-04 12:51:49 +0800

    tijiao

---
+++ myapplication/app/src/main/res/Test/Test_one.java
@@ -0,0 +1,10 @@
+package Test;
+
+/**
+ * Created by dell on 2017/2/22.
+ */
+
+public class Test_one {
+
+
+}
---
+++ myapplication/myproject/src/main/java/com/example/MyClass.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class MyClass {
+    public static void main(String args[]){
+        int y = getadd(6,8);
+        system.out.print(y);
+    }
+
+
+
+    public static int getadd(int a,int b){
+        return a*b;
+    }
+}
